341e847e6090f2b1b9f699cfabde493021227c75
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-12-08 21:22:24 -0300

    Resolvendo o problema de identificação do sistema operacional (by Natyvit)

--- django_full_crud/globals/main.py
+++ django_full_crud/globals/main.py
@@ -1,6 +1,9 @@
-from pathlib import Path
-import os
 import json
+import os
+import platform
+from pathlib import Path
+
+so = platform.system()
 
 
 def update_current_app(app_name):
@@ -13,8 +16,12 @@ def base_dir(name=None):
 
 
 def get_project_name():
-    windows_path = os.path.join(PROJECT_BASE_DIR, ".vscode", "django_full_crud.json")
-    linux_path = os.path.join(PROJECT_BASE_DIR.parent, ".vscode", "django_full_crud.json")
+    windows_path = os.path.join(
+        PROJECT_BASE_DIR, ".vscode", "django_full_crud.json"
+    )
+    linux_path = os.path.join(
+        PROJECT_BASE_DIR.parent, ".vscode", "django_full_crud.json"
+    )
 
     if os.path.exists(windows_path):
         path = windows_path
@@ -24,19 +31,32 @@ def get_project_name():
     try:
         return get_django_full_crud_json(path)
     except:
-        raise (Exception("Arquivo django_full_crud.json não encontrado, por favor, verifique a seção 'Modo de uso' da documentação"))
+        raise (
+            Exception(
+                "Arquivo django_full_crud.json não encontrado, por favor, verifique a seção 'Modo de uso' da documentação"
+            )
+        )
+
 
 def get_django_full_crud_json(path):
     with open(path, encoding="utf-8") as settings:
         dict_json = json.load(settings)
         return dict_json.get("project_name", None)
 
+
 def get_project_dir(string=""):
-    return os.path.join(PROJECT_BASE_DIR, PROJECT_NAME, string)
+    if so == "Windows":
+        return os.path.join(PROJECT_BASE_DIR, PROJECT_NAME, string)
+    elif so == "Linux":
+        return os.path.join(PROJECT_BASE_DIR.parent, PROJECT_NAME, string)
+    else:
+        raise (Exception("SO não disponível"))
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent.parent
+PROJECT_BASE_DIR = (
+    Path(__file__).resolve().parent.parent.parent.parent.parent.parent
+)
 
 PROJECT_NAME = get_project_name()
 
