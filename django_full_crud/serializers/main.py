fda58f0ff80b2c21fefa04236aa6afa1e0875bb1
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-12-10 11:29:37 -0300

    Configs dos inits

--- django_full_crud/serializers/main.py
+++ django_full_crud/serializers/main.py
@@ -1,12 +1,14 @@
 import django_full_crud.scripts as scripts
-from django_full_crud.globals.main import get_project_dir
+from django_full_crud.globals import get_django_full_crud_json, get_project_dir
 
 
 def create_serializers_files(app_name, snake_model_name, model_name):
-    serializer_script = scripts.serializer_script(model_name)
-    with open(f"{get_project_dir(app_name)}/serializers/{snake_model_name}_serializer.py", "w", encoding="utf-8") as serializer:
-        serializer.write(serializer_script)
+    if get_django_full_crud_json("make_serializers"):
+        serializer_script = scripts.serializer_script(model_name)
+        with open(f"{get_project_dir(app_name)}/serializers/{snake_model_name}_serializer.py", "w", encoding="utf-8") as serializer:
+            serializer.write(serializer_script)
 
-    init_script = scripts.init_script(app_name, "serializers")
-    with open(f"{get_project_dir(app_name)}/serializers/__init__.py", "w", encoding="utf-8") as init:
-        init.write(init_script)
+        if get_django_full_crud_json("make_serializers_init"):
+            init_script = scripts.init_script(app_name, "serializers")
+            with open(f"{get_project_dir(app_name)}/serializers/__init__.py", "w", encoding="utf-8") as init:
+                init.write(init_script)
