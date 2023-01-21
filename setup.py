83e95a7879586cfef1816ccae7247a2351da0e11
flaviotech <flavio.nogueira.profissional@gmail.com>
2023-01-21 15:58:17 -0300

    Versão 0.2.8.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setup(
     name="django_full_crud",
-    version="0.2.8.2",
+    version="0.2.8.3",
     url="https://github.com/flaviotech/django-full-crud/",
     license="MIT License",
     author="Flávio Silva",
