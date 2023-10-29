24655e17906b30e8c9c39c49ff98938ee9f93ec3
flavionogueiraa <flavio.nogueira.profissional@gmail.com>
2023-10-29 16:40:37 -0300

    Viewset

--- django_full_crud/scripts/viewset_script.py
+++ django_full_crud/scripts/viewset_script.py
@@ -1,9 +1,30 @@
+# flake8: noqa
 from importlib import import_module
 
+from django_full_crud.globals import get_django_full_crud_json
 from django_full_crud.utils import get_prop
 
 
 def viewset_script(app_name, model_name, snake_model_name):
+    simple_viewset = get_django_full_crud_json("simple_viewset", False)
+    viewset_imports = get_django_full_crud_json("viewset_imports", "")
+    replaced_viewset_imports = [
+        viewset_import.replace("'", "") + "\n"
+        for viewset_import in viewset_imports
+    ]
+    if simple_viewset:
+        return f"""from rest_framework import viewsets
+{default_join(replaced_viewset_imports)}
+
+from ..models import {model_name}
+from {app_name}.serializers import {model_name}Serializer
+
+
+class {model_name}ViewSet({get_django_full_crud_json("viewset_superclass", "viewsets.ModelViewSet")}):
+    queryset = {model_name}.objects.all()
+
+    serializer_class = {model_name}Serializer
+"""
     module = import_module(f"{app_name}.models.{snake_model_name}")
     class_object = getattr(module, f"{model_name}")
 
@@ -19,7 +40,7 @@ from ..models import {model_name}
 from {app_name}.serializers import {model_name}Serializer
 
 
-class {model_name}ViewSet(viewsets.ModelViewSet):
+class {model_name}ViewSet({get_django_full_crud_json("viewset_superclass", "viewsets.ModelViewSet")}):
     queryset = {model_name}.objects.all()
 
     serializer_class = {model_name}Serializer
